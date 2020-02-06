808df0b277e0a4d391deba3a58b69a5526f7b899
Parieiko Vlad <parieiko.vlad@gmail.com>
2020-02-06 22:32:04 +0300

    Main js file. Slider setting and responsive menu setting

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.next-series',
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 2,
+        }
+    }
+});
+
+let menuButton = document.querySelector('.menu-button');
+
+let menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', function() {
+   menuButton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
+})
